f893eaf490785e5ebe2342f3e0690034e3119eda
shin-ga-eun <sge7102@naver.com>
2020-02-15 20:57:22 +0900

    헤더 수정.
    로그인, 회원가입 버튼만 추가

--- src/pages/myhealthcheck/layout/header.js
+++ src/pages/myhealthcheck/layout/header.js
@@ -1,21 +1,11 @@
 import clsx from "clsx";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Drawer from "@material-ui/core/Drawer";
-import Box from "@material-ui/core/Box";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import Badge from "@material-ui/core/Badge";
-import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-
 import Link from "@material-ui/core/Link";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import NotificationsIcon from "@material-ui/icons/Notifications";
+import Button from "@material-ui/core/Button";
 import useStyles from "../style/useStyles";
 
 import React, { useState } from 'react';
@@ -41,15 +31,30 @@ const Header = () => {
         className={clsx(classes.appBar, open && classes.appBarShift)}
       >
         <Toolbar className={classes.toolbar}>
-          <Typography
-            component="h1"
-            variant="h6"
-            color="inherit"
-            noWrap
-            className={classes.title}
-          >
-            내 건강관리
-          </Typography>
+          <Grid item xs={10}>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              className={classes.title}
+            >
+              내 건강관리
+            </Typography>
+          </Grid>
+          <Grid itme xs={1}>
+            {/* <Link to="/login" color="in"> */}
+              <Button color="inherit">로그인</Button>
+            {/* </Link>    */}
+            {/* <Link to="/login" color="in"> */}
+            <Button color="inherit">로그아웃</Button>
+            {/* </Link>    */}
+          </Grid>
+          <Grid itme xs={1}>
+            <Link to="/signIn" color="inherit">
+                <Button color="inherit">회원가입</Button>
+              </Link>   
+          </Grid>
         </Toolbar>
       </AppBar>
     </div>
